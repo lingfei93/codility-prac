c6ad92c90216e81f080a6f5a8ce3274d702929dc
Lingfei <lren002@berkeley.edu>
2018-01-04 16:07:30 +0800

    Second commit

---
+++ c/Codility/Codility/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Codility
+//
+//  Created by Ren Ling Fei on 4/1/18.
+//  Copyright © 2018 Ren Ling Fei. All rights reserved.
+//
+
+#include <stdio.h>
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hjjello, World!\n");
+    return 0;
+}
